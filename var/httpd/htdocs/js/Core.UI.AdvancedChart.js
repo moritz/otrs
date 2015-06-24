774f972eb3383b1632fd9a8be5c6d73024cfecef
Martin Gruner <martin.gruner@otrs.com>
2015-06-24 12:24:27 +0200

    Improved SVG2PNG.

--- var/httpd/htdocs/js/Core.UI.AdvancedChart.js
+++ var/httpd/htdocs/js/Core.UI.AdvancedChart.js
@@ -648,45 +648,32 @@ Core.UI.AdvancedChart = (function (TargetNS) {
      * @memberof Core.UI.AdvancedChart
      * @function
      * @param {jQueryObject} $SVGContainer - The element containing the SVG element. There should be no other content.
-     * @param {function} FinishCallback - This function will be called when the conversion is finished.
-     *      It receives the data URL for the PNG as a string as function argument.
+     * @return {String} - The base64 data URL containing the PNG data.
      * @description
      *      Convert an SVG element to a PNG data URL.
      */
-    TargetNS.ConvertSVGtoPNG = function($SVGContainer, FinishCallback) {
+    TargetNS.ConvertSVGtoPNG = function($SVGContainer) {
 
         var SVGContent = $SVGContainer.html().trim(), // Canvg requires trimmed content
-            InitialRedrawDone = false,
-            $CanvasContainer;
+            $CanvasContainer,
+            $Canvas;
 
-        $CanvasContainer = $('<div style="position: absolute; top: 0; visibility: hidden;"></div>')
-            .css('height', $SVGContainer.css('height'))
-            .css('width', $SVGContainer.css('width'))
-            .append('<canvas></canvas>')
+        $Canvas = $('<canvas></canvas>')
+            .attr('height', $SVGContainer.css('height'))
+            .attr('width', $SVGContainer.css('width'));
+
+        $CanvasContainer = $('<div style="position: absolute; top: 0; visibility: visible;"></div>')
+            .append($Canvas)
             .appendTo('body');
 
-        /* This function a) forces another redraw after the first frame as canvg does not draw the full element
-            in the first place, and b) hands over the PNG data to the FinishCallback function.
-        */
-        function HandleRedraw() {
-            // Code for first frame - force redraw.
-            if (!InitialRedrawDone) {
-                InitialRedrawDone = true;
-                return true;
-            }
-
-            // Code for second frame
-            $CanvasContainer.find('canvas').get(0).svg.stop();
-            FinishCallback( $CanvasContainer.find('canvas').get(0).toDataURL('image/png') );
-            $CanvasContainer.find('canvas').remove();
-            return false;
-        }
-        // Draw svg on canvas
         canvg(
-            $CanvasContainer.find('canvas').get(0),
+            $Canvas.get(0),
             SVGContent,
-            { ignoreMouse: true, ignoreAnimations: true, forceRedraw: HandleRedraw }
+            { ignoreMouse: true, ignoreAnimations: true }
         );
+        $Canvas.get(0).svg.stop();
+        $CanvasContainer.remove();
+        return $Canvas.get(0).toDataURL('image/png');
     };
 
     return TargetNS;
